f31e61a492da251cf15a894f19ea4a1413124245
Osvaldo-1 <robleshdzeder@gmail.com>
2025-04-19 19:05:35 -0600

    instalacion de react, configuracion supabase react

---
+++ barbershop/src/supabaseClient.js
@@ -0,0 +1,9 @@
+// Importar la función createClient de Supabase
+import { createClient } from '@supabase/supabase-js';
+
+// Reemplazar con tu URL y clave pública de Supabase
+const supabaseUrl = 'https://aomgndsiwnnceituinzw.supabase.co'; // Tu URL
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFvbWduZHNpd25uY2VpdHVpbnp3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDUxMDgxMzAsImV4cCI6MjA2MDY4NDEzMH0.IcDerXsHIpjIzzYL7hH2VuPRIeyR6nKT9-0V0NnEfC4'; // Tu clave pública (anon key)
+
+// Crear el cliente de Supabase
+export const supabase = createClient(supabaseUrl, supabaseKey);
