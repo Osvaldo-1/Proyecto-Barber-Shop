ede35cf515fbe06aa670936842cb3c51514ddb07
aceraspire06 <aceraspire06@TLC.COM>
2025-05-09 15:26:55 -0600

    se dejo como estaba antes carousel

--- barbershop/src/pages/Appointment.jsx
+++ barbershop/src/pages/Appointment.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import '../Styles/appointment.css'
+import '../Styles/appointment.css';
 import { fetchData, insertData } from '../supabaseService.js';
 
 
