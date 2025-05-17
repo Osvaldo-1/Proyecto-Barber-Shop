bf5e4da912011f702dc5dce91de575dcc5cf6695
aceraspire06 <aceraspire06@TLC.COM>
2025-05-17 12:51:28 -0600

    avance de logica de admin

--- barbershop/src/App.js
+++ barbershop/src/App.js
@@ -17,34 +17,50 @@ import Adminhome from './pages/Adminhome.jsx';
 import AdminDashboard from './pages/Adminhome.jsx';
 import { Comentario } from './pages/Comentario.jsx';
 import StatusCita from './pages/StatusCita.jsx';
+
+import AdminLayout from './layouts/Adminlayout';
+
+import DashboardOverview from './components/AdminDashboardOverview';
+import AppointmentsList from './components/AdminAppointmentsList';
+import ServicesManagement from './components/AdminServicesManagement';
+import UsersManagement from './components/AdminUsersManagement';
+import Settings from './components/AdminSettings';
 function AppRoutes() {
   const { user } = useAuth();
+  const location = useLocation();
+
+  const isAdminRoute = location.pathname.startsWith('/admin');
 
   return (
     <>
       <Navbar />
       <Routes>
-        <Route path="/admin/*" element={<AdminDashboard />} />
+
+        {/*/Rutas publicas */}
+        <Route path="/" element={<Home />} />
         <Route path="/home" element={<Home />} />
         <Route path="/comentario" element={<Comentario />} />
         <Route path="/status" element={user ? <StatusCita /> : <Navigate to="/login" />} />
-        <Route path="/adminhome" element={<Adminhome />} />
         <Route path="/services" element={<Services />} />
         <Route path="/pricing" element={<Pricing />} />
         <Route path="/about" element={<About />} />
         <Route path="/gallery" element={<Gallery />} />
         <Route path="/contact" element={<Contact />} />
-        <Route
-          path="/appointment"
-          element={user ? <Appointment /> : <Navigate to="/login" />}
-        />
+        <Route path="/appointment" element={user ? <Appointment /> : <Navigate to="/login" />} />
         <Route path="/register" element={<Register />} />
         <Route path="/login" element={<Login />} />
+
+          <Route path="/admin" element={<AdminLayout />}>
+          <Route index element={<DashboardOverview />} />
+          <Route path="appointments" element={<AppointmentsList />} />
+          <Route path="services" element={<ServicesManagement />} />
+          <Route path="users" element={<UsersManagement />} />
+          <Route path="settings" element={<Settings />} />
+        </Route>
       </Routes>
     </>
   );
 }
-
 function App() {
   return (
     <AuthProvider>
