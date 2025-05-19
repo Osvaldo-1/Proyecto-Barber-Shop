c084d770552401c0bedcbc31aa2d353d6d70ec19
aceraspire06 <aceraspire06@TLC.COM>
2025-05-19 11:47:08 -0600

    boton de cerrar sesion admin

--- barbershop/src/components/AdminSidebar.jsx
+++ barbershop/src/components/AdminSidebar.jsx
@@ -1,8 +1,18 @@
 import { Nav } from 'react-bootstrap';
+import { useAuth } from '../context/AuthContext'; 
 
 
 
 function Sidebar() {
+    const { logout } = useAuth();
+
+  const handleLogout = async () => {
+    try {
+      await logout();
+    } catch (error) {
+      console.error("Error al cerrar sesión:", error.message);
+    }
+  };
   return (
     <div className="sidebar">
       <h5 className="mb-3 bg">Navegación</h5>
@@ -21,6 +31,12 @@ function Sidebar() {
         </Nav.Item>
         <Nav.Item>
           <Nav.Link href="#" className='text-light'><span className='status'>Configuración</span></Nav.Link>
+        </Nav.Item>
+        {/* Botón de Cerrar Sesión */}
+        <Nav.Item className="mt-4">
+          <Nav.Link onClick={handleLogout} className='text-danger'>
+            <span className='status'>Cerrar Sesión</span>
+          </Nav.Link>
         </Nav.Item>
       </Nav>
     </div>
